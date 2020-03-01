185cec8e99f6c27144c8fad0c66f20e999364efb
bittichn <nordine.bittich@cogni.zone>
2020-03-01 17:35:06 +0100

    fixes

--- install.sh
+++ install.sh
@@ -13,15 +13,6 @@ docker volume prune -f
 docker network prune -f
 docker-compose up -d --force-recreate
 
-echo "sleep 60s"
-
-sleep 60s # wait for kafka, elasticsearch & virtuoso then restart the containers
-
-docker restart docker_atrianglefilesink_1
-docker restart docker_atrianglerdfsink_1
-docker restart docker_atriangleeventdispatcher_1
-docker restart docker_atrianglerest_1
-docker restart docker_atriangleelasticsink_1
 
 echo "ready to use! Containers will restart until they are up, but check the containers using: "
 echo "docker ps"
