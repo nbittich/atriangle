8986c38cee388f8a4d6a5979e964c22e2b4e1da2
bittichn <nordine.bittich@cogni.zone>
2020-03-08 06:46:52 +0100

    refactoring before file ingest service

--- core/src/main/java/tech/artcoded/atriangle/core/config/ZookeeperDiscoveryConfig.java
+++
@@ -1,9 +0,0 @@
-package tech.artcoded.atriangle.core.config;
-
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@EnableDiscoveryClient
-public class ZookeeperDiscoveryConfig {
-}
--- rest/src/main/java/tech/artcoded/atriangle/rest/RestApplication.java
+++ rest/src/main/java/tech/artcoded/atriangle/rest/RestApplication.java
@@ -2,14 +2,15 @@ package tech.artcoded.atriangle.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Import;
 import tech.artcoded.atriangle.core.config.KafkaConfig;
-import tech.artcoded.atriangle.core.config.ZookeeperDiscoveryConfig;
 
 @SpringBootApplication
-@Import({KafkaConfig.class, ZookeeperDiscoveryConfig.class})
+@Import({KafkaConfig.class})
 @EnableZuulProxy
+@EnableDiscoveryClient
 public class RestApplication {
   public static void main(String[] args) {
     SpringApplication.run(RestApplication.class, args);
