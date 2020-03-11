8906ec06a2b262f25d4fe4bb2ab4219873ad4c4d
bittichn <nordine.bittich@cogni.zone>
2020-03-11 18:46:17 +0100

    some refactoring

--- core-kafka/src/main/java/tech/artcoded/atriangle/core/kafka/NamedBean.java
+++
@@ -1,6 +0,0 @@
-package tech.artcoded.atriangle.core.kafka;
-
-public interface NamedBean {
-  String OBJECT_MAPPER_WRAPPER = "coreObjectMapperWrapper";
-  String LOGGER_ACTION = "coreLoggerAction";
-}
