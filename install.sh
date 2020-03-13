74bc70a909500eb2b58990919c0189d960ce4d8e
bittichn <nordine.bittich@cogni.zone>
2020-03-13 06:07:10 +0100

    add docker conf

--- install.sh
+++ install.sh
@@ -11,15 +11,19 @@ mvn clean install -Ddocker
 docker-compose -f docker/docker-compose.yml up -d --force-recreate
 
 echo "ready to use! Containers will restart until they are up, but check the containers using: "
+
 echo "docker ps"
+
 echo "docker logs -f docker_atrianglerestsink_1"
 echo "docker logs -f docker_atrianglelogsink_1"
 echo "docker logs -f docker_atrianglemongodbsink_1"
 echo "docker logs -f docker_atrianglerdfsink_1"
 echo "docker logs -f docker_atriangleeventdispatcher_1"
+
 echo "docker logs -f docker_atrianglerest_1"
 echo "docker logs -f docker_atriangleelasticsink_1"
 echo "docker logs -f docker_atriangleuploadrest_1"
+echo "docker logs -f docker_atriangleprojectrest_1"
 
 
 
