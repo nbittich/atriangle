2f5281ae85ff9f33cb2889266e1a6792ef1f27e6
bittichn <nordine.bittich@cogni.zone>
2020-03-16 11:49:16 +0100

    cleanup gateway

--- web/rest-gateway/src/main/java/tech/artcoded/atriangle/rest/RestProxyGatewayApplication.java
+++ web/rest-gateway/src/main/java/tech/artcoded/atriangle/rest/RestProxyGatewayApplication.java
@@ -4,12 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.context.annotation.Import;
-import tech.artcoded.atriangle.core.kafka.KafkaConfig;
-import tech.artcoded.atriangle.core.rest.config.SwaggerConfig;
 
 @SpringBootApplication
-@Import({KafkaConfig.class, SwaggerConfig.class})
 @EnableZuulProxy
 @EnableDiscoveryClient
 public class RestProxyGatewayApplication {
