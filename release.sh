d4dd83ae806acf434ba114b144c323adb263f3e9
bittichn <nordine.bittich@cogni.zone>
2020-03-22 10:28:14 +0100

    RELEASE SCRIPT

--- release.sh
+++ release.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
 set -e # fail script on error
 
-if [[ -z "$1" || -z "$2" ]]
+releaseVersion=$1
+nextVersion=$2-SNAPSHOT
+
+if [[ -z "$releaseVersion" || -z "$nextVersion" ]]
 then
   echo "version mut be provided"
   exit -1;
 fi
 
-releaseVersion = $1
-nextVersion = $2-SNAPSHOT
  echo "release" $releaseVersion ", next" $nextVersion
+
 
 mvn --batch-mode -Dtag=$releaseVersion release:prepare \
                  -DreleaseVersion=$releaseVersion \
